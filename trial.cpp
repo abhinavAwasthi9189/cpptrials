49ce325239ffa0110d6a7b389b91117c4fc1cae3
abhinavAwasthi9189 <awasthiabhinav2006@gmail.com>
2026-07-30 18:01:47 +0530

    cpp testing -3

--- trial.cpp
+++ trial.cpp
@@ -1,26 +1,54 @@
 #include <iostream>
-//using namespace std; NO GOOD PRACTICE TO DO CAN CAUSE ERROR
 
 using std::cout;
 using std::cin;
 
 #define  nn std::endl;
 
+int num = 5;
+
 //we have created overloading functions here, same name different variables.
-int area( int radius){ return (radius*radius);}
+int area( int radius){ return (radius*num);}
 int area( int l, int b){ return (l*b);}
 
 int main(){
-
-    int l;
-    cin >> l;
-    int b;
-    cin >> b;
-
-    int k1=area(l);
-    int k2=area(l,b);
-
-    cout << k1 << " " << k2 << nn;
+    int num;
+    cin >> num;
     
+    cout << num << nn;
+
+    // writting '::' before a variable name induces its global value
+    cout << ::num<< nn;
+
+    /*int k;
+    cin >> k;
+    cout << ::k << nn;
+    i added this think to see if a variable is called globally but only exisits locally can it be called like this.
+    it tells its wrong before compilation
+    */
+
+    
+    cout << area(4) << nn;
+
+    int arr[] = {1,2,3,4,5,6};
+    int ar[] = {};
+    int a[6];
+
+    cout << sizeof(ar) << nn;
+    cout << sizeof(a) << nn;
+    cout << sizeof(arr) << nn;
+
+    for( int i =0; i<6;i++){
+        cin >> a[i];
+    }
+    
+    cout << nn;
+
+    // .....for each loop.....
+    for(int i : a){
+        cout << i << nn;
+    }
+    
+
     return 0;
 }
