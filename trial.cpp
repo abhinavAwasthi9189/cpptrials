d5c56ec8f4d53570a20b1dec233dd0fa408268ac
abhinavAwasthi9189 <awasthiabhinav2006@gmail.com>
2026-08-01 14:15:03 +0530

    OOPS. i know most of  it already. just the syntax needed

--- trial.cpp
+++ trial.cpp
@@ -5,36 +5,46 @@ using std::cin;
 using std::string;
 #define  nn std::endl;
 
+//we can create an name = int connecttion so that with enum when the name comes the value we get the the int.
+enum value{one = 1, three = 3, fourteen = 14};
 
-struct coordinates{int x;int y;};
+class git{
+    // we create python like indentation and inside which we write which are public and which are private.
+    public:
+        int reponum;
+        string name;
+        
 
-void printcord(coordinates A){
-    std::swap(A.x,A.y);
-    cout << A.x << nn;
-    cout << A.y << nn;
-}
-
-void printcoord(coordinates &A){
-    std::swap(A.x,A.y);
-    cout << A.x << nn;
-    cout << A.y << nn;
-}
+    private:
+        string password;
+    
+    public:
+        git(string naam, int val){name = naam; reponum = val;}
+        void gitadd(){reponum++;}
+        void get_passwd(string paswd){password = paswd;}
+        bool check(string passwd){return password==passwd;}
+};
 
 int main(){
+    
+    //so we give a value of enume here. same as struct in most casses but we can do equals to with both types
+    value x = one;
+    
 
-    //unlike c in cpp we only write struct name not whole struct name thing.
-    coordinates A;
-    A.x = 5;
-    A.y=7;
+    // we can use both the condition the name or the int.
+    if(x==one){cout << "true" <<nn;}
+    if(x==1){cout << "true" <<nn;}
 
-    //this is all about passing by value and passing by reference. one the changes made be also in the original value. other not.
-    printcord(A);
-    cout << A.x << nn;
-    cout << A.y << nn;
-    printcoord(A);
-    cout << A.x << nn;
-    cout << A.y << nn;
+    git abhinav("abhinav",10);
+    cout << abhinav.name << " " << abhinav.reponum << nn;
+    abhinav.get_passwd("peigon");
+    cout << abhinav.check("yctyv") << "  " << abhinav.check("peigon") << nn;
+    abhinav.gitadd();
+    abhinav.gitadd();
+    abhinav.gitadd();
+    cout << abhinav.name << " " << abhinav.reponum << /*abhinav.password <<*/nn;
 
+    return 0;
 }
 
 
